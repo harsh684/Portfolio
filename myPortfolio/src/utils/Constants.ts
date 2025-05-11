266d3e2ee3448a5ff8061d5663b56a775ae40068
Harsh dangi <76767370+harsh684@users.noreply.github.com>
2025-05-11 10:00:25 +0530

    Update Constants.ts

--- myPortfolio/src/utils/Constants.ts
+++ myPortfolio/src/utils/Constants.ts
@@ -181,15 +181,15 @@ import {
   const testimonials = [
     {
       testimonial:
-        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-      name: "Sara Lee",
-      designation: "CFO",
-      company: "Acme Co",
+        "I thought it was impossible to make a website as beautiful as our product, but Harsh proved me wrong.",
+      name: "Ayush Kumar",
+      designation: "Product Manager",
+      company: "Alt Mobility",
       image: "https://randomuser.me/api/portraits/women/4.jpg",
     },
     {
       testimonial:
-        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+        "I've never met a web developer who truly cares about their clients' success like Harsh does.",
       name: "Chris Brown",
       designation: "COO",
       company: "DEF Corp",
@@ -197,7 +197,7 @@ import {
     },
     {
       testimonial:
-        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+        "After Harsh optimized our website, our traffic increased by 50%. We can't thank them enough!",
       name: "Lisa Wang",
       designation: "CTO",
       company: "456 Enterprises",
